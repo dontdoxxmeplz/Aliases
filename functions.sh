795489bee02ddb61e44a3087d31d40e86efee4cb
dontdoxxmeplz <85412253+dontdoxxmeplz@users.noreply.github.com>
2021-11-11 08:51:14 -0500

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -46,3 +46,28 @@ fi
 }
 
 IFS=$SAVEIFS
+
+
+#compdef kubectx kctx=kubectx
+
+local KUBECTX="${HOME}/.kube/kubectx"
+PREV=""
+
+local context_array=("${(@f)$(kubectl config get-contexts --output='name')}")
+local all_contexts=(\'${^context_array}\')
+
+if [ -f "$KUBECTX" ]; then
+    # show '-' only if there's a saved previous context
+    local PREV=$(cat "${KUBECTX}")
+
+    _arguments \
+      "-d:*: :(${all_contexts})" \
+      "(- *): :(- ${all_contexts})"
+else
+    _arguments \
+      "-d:*: :(${all_contexts})" \
+      "(- *): :(${all_contexts})"
+fi
+
+#compdef kubens kns=kubens
+_arguments "1: :(- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}'))"
